221d80a81aac56b92943cdf576a1f258866cf67e
vasilets1998 <47006146+vasilets1998@users.noreply.github.com>
2020-12-24 17:29:06 +0200

    del any in url

--- client/src/utils/urls.ts
+++ client/src/utils/urls.ts
@@ -1,6 +1,6 @@
 import { createPath } from "rd-url-utils";
 
 export const ROOT_URL = createPath("/");
-export const BOOKS = createPath<any>("/books");
+export const BOOKS = createPath("/books");
 export const BOOK = createPath<{ id: string }>("/book/:id");
 export const SHOPING = createPath("/shoping");
